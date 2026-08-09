0848452daadfc26e97f26f7521688ac3321f2a6c
Afsal Muhammad <afsalmuhammad0718@gmail.com>
2026-08-09 19:45:35 +0700

    Update latest and older articles in script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const latestArticle = {
-  title: "10 Tanda Hidupmu Akan Semakin Hancur yang Sering Tidak Disadari",
-  url: "https://afsalmuhammad.web.id/10-tanda-hidupmu-akan-semakin-hancur-yang-sering-tidak-disadari/",
-  date: "2026-08-06",
+  title: "10 Ciri-Ciri Otak Kamu Sudah Rusak dan Perlu Direset",
+  url: "https://afsalmuhammad.web.id/10-ciri-ciri-otak-kamu-sudah-rusak-dan-perlu-direset/",
+  date: "2026-08-10",
 };
 
 const olderArticles = [
   {
+    title: "10 Tanda Hidupmu Akan Semakin Hancur yang Sering Tidak Disadari",
+    url: "https://afsalmuhammad.web.id/10-tanda-hidupmu-akan-semakin-hancur-yang-sering-tidak-disadari/",
+    date: "2026-08-03",
+  },
+  {
     title: "7 Cara Mengubah Hidup Hanya yang Bisa Dilakukan Hari Ini",
     url: "https://afsalmuhammad.web.id/7-cara-mengubah-hidup-hanya-yang-bisa-dilakukan-hari-ini/",
-    date: "2026-08-03",
+    date: "2026-08-06",
   },
   {
     title: "10 Tanda Kehilangan Arah yang Sering Tidak Disadari",
